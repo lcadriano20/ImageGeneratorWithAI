ec16a27802dc887338e6dadaac21270bdc612e16
Adriano Lima Silva de Carvalho <adriano.decarvalho@dpu.def.br>
2023-12-04 10:49:35 -0300

    Adicionei arquivos iniciais

---
+++ script.js
@@ -0,0 +1,11 @@
+// Variable Creation 
+const button = document.querySelector('.submit')
+const imageContainer = document.querySelector('.imageContainer')
+const form = document.querySelector('form')
+
+
+
+
+async function grabImage() {
+    
+}
